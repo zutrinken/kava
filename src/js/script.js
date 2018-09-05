48d7af540eafbebc4c332c45497fd3b0b1feae03
zutrinken <xoxo@zutrinken.com>
2018-09-05 20:43:01 +0200

    Ghost 2.0 update

--- src/js/script.js
+++ src/js/script.js
@@ -74,6 +74,22 @@ viewport.on({
 	highlight();
 
 	/* ==========================================================================
+	   Gallery
+	   ========================================================================== */
+
+	function gallery() {
+		var images = document.querySelectorAll('.kg-gallery-image img');
+		images.forEach(function (image) {
+			var container = image.closest('.kg-gallery-image');
+			var width = image.attributes.width.value;
+			var height = image.attributes.height.value;
+			var ratio = width / height;
+			container.style.flex = ratio + ' 1 0%';
+		});
+	}
+	gallery();
+
+	/* ==========================================================================
 	   Fitvids
 	   ========================================================================== */
 
